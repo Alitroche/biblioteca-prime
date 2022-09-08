ca4824a1b39d3bf539149054fb422defc8fc495d
Alicia Troche <trochea906@gmail.com>
2022-09-08 17:49:37 -0400

    Tarea 1 Angular(Parte 3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { LibrosComponent } from './libros/libros.component';
 import { AutoresComponent } from './autores/autores.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +20,9 @@ import { AutoresComponent } from './autores/autores.component';
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    HttpClientModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
